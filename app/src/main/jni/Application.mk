7028ac693bf7a209ff3318794daae72a7d872712
duongnguyennalry-lang <duongnguyennalry@gmail.com>
2026-08-18 13:48:14 +0700

    Update Application.mk

--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -1,7 +1,6 @@
-# To AIDE Users: If you are using 32-bit/ARMv7 phone, please remove arm64-v8a
-APP_ABI := armeabi-v7a
-# APP_PLATFORM := android-18 #APP_PLATFORM does not need to be set. It will automatically defaulting
-APP_STL := c++_static
-APP_OPTIM := release
+APP_ABI          := arm64-v8a
+APP_PLATFORM     := android-24
+APP_STL          := c++_static
+APP_OPTIM        := release
 APP_THIN_ARCHIVE := true
-APP_PIE 		:= true
+APP_PIE          := true
